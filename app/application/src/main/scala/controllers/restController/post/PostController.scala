e85bd4884b19e778c5eb67d3bba54e35861595db
thanh_lm <thanh_lm@flinters.vn>
2022-08-05 14:45:10 +0700

    update PostController

--- app/application/src/main/scala/controllers/restController/post/PostController.scala
+++ app/application/src/main/scala/controllers/restController/post/PostController.scala
@@ -1,13 +1,29 @@
 package controllers.restController.post
 
-
+import domain.src.main.scala.model.post.Post
+import domain.src.main.scala.services.post.PostService
+import dto.post.ListPostResponse
+import play.api.libs.json.Json
 import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
 
 import javax.inject.{Inject, Singleton}
-
+import scala.util.{Failure, Success, Try}
+import helper.jsonFormat.post.ListPostJsonFormat._
 @Singleton
-class PostController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
-  def index: Action[AnyContent] = Action {
-    Ok("ok")
+class PostController @Inject()(val controllerComponents: ControllerComponents, postService: PostService) extends BaseController {
+  def getAllPostWithPagination(page: Int, size: Int): Action[AnyContent] = Action {
+    Try {
+      val listPost: List[Post] = postService.getAllPostWithPagination(page, size)
+      val totalPost: Long = postService.getPostCount
+      ListPostResponse(
+        posts = listPost,
+        count = listPost.length,
+        total = totalPost
+      )
+    }
+    match {
+      case Success(value) => Ok(Json.toJson(value))
+      case Failure(exception) => BadRequest(exception.toString)
+    }
   }
 }
