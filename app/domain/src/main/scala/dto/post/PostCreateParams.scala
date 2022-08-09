987068549abbebdca003d57bc18a6560b4558d42
thanh_lm <thanh_lm@flinters.vn>
2022-08-09 17:15:13 +0700

    remove bad practice

--- app/domain/src/main/scala/dto/post/PostCreateParams.scala
+++ app/domain/src/main/scala/dto/post/PostCreateParams.scala
@@ -6,10 +6,4 @@ case class PostCreateParams(
                            previewContent: String,
                            author: String,
                            thumbnail: String
-                           ) {
-  if (title.isEmpty) throw new IllegalArgumentException("Title is required")
-  if (content.isEmpty) throw new IllegalArgumentException("Content is required")
-  if (previewContent.isEmpty) throw new IllegalArgumentException("Preview Content is required")
-  if (author.isEmpty) throw new IllegalArgumentException("Author is required")
-  if (thumbnail.isEmpty) throw new IllegalArgumentException("Thumbnail is required")
-}
+                           )
